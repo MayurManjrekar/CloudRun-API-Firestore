576ef92d08dd62e4ba0dcca1e72d647a859a8b1e
Mayur Manjrekar <102725772+Mayur-1999@users.noreply.github.com>
2023-09-01 12:30:00 +0530

    Update variable.tf

--- second-deployment/modules/CloudRun-API-Gateway/variable.tf
+++ second-deployment/modules/CloudRun-API-Gateway/variable.tf
@@ -4,30 +4,42 @@ variable "project_id" {
 
 variable "region" {
   description = "name of specific region to deploy services into, e.g. eu-west1"
-  default     = "europe-west1"
+  default     = "us-central1"
+  type        = string 
 }
 
 variable "api_gateway_name" {
   description = "gateway name"
+  type = string 
   default     = "Bookshelf API Gateway"
 }
 
 variable "api_id" {
   description = "gateway ID"
+  type        = string 
   default     = "bookshelf-api-id"
 }
 
 variable "config_name" {
   description = "config name"
+  type        = string 
   default     = "Bookshelf API Config"
 }
 
 variable "gateway_id" {
   description = "gateway ID"
+  type        = string 
   default     = "bookshelf-gateway-id"
 }
 
 variable "gateway_name" {
   description = "gateway ID"
+  type        = string 
   default     = "Bookshelf Gateway"
+}
+
+
+variable "config_file_path" {
+  description = "config file path"
+  type        = string 
 }
