8df1e617c6fd9b7b5f00eb7c93597296fba3ba06
Mayur Manjrekar <102725772+Mayur-1999@users.noreply.github.com>
2023-09-01 12:28:02 +0530

    Update main.tf

--- second-deployment/modules/CloudRun-API-Gateway/main.tf
+++ second-deployment/modules/CloudRun-API-Gateway/main.tf
@@ -1,44 +1,28 @@
-#resource "google_project_service" "api-gateway" {
-#  project = var.project_id
-#  service = "apigateway.googleapis.com"
-#}
-
-#resource "google_project_service" "service-management" {
-#  project = var.project_id
-#  service = "servicemanagement.googleapis.com"
-#}
-
-#resource "google_project_service" "service-control" {
-#  project = var.project_id
-#  service = "servicecontrol.googleapis.com"
-#}
-
 resource "google_api_gateway_api" "api_gw" {
-  project               = var.project_id
+  project      = var.project_id
   provider     = google-beta
   api_id       = var.api_id  #"api-id-tf"
   display_name = var.api_gateway_name  #"API Gateway"
-  #depends_on   = [google_project_service.api-gateway , google_project_service.service-management , google_project_service.service-control]
 }
 
 resource "google_api_gateway_api_config" "api_cfg" {
   project               = var.project_id
-  provider      = google-beta
-  api           = google_api_gateway_api.api_gw.api_id
+  provider              = google-beta
+  api                   = google_api_gateway_api.api_gw.api_id
   #api_config_id_prefix = "api"
-  display_name  = var.config_name   #"Api Config"
+  display_name          = var.config_name   #"Api Config"
   openapi_documents {
     document {
       path     = var.config_file_path #"spec.yaml"
-      contents = filebase64("spec.yaml")
+      contents = var.contents         #filebase64("spec.yaml")
     }
   }
 }
 
 resource "google_api_gateway_gateway" "gw" {
-  project               = var.project_id
-  provider   = google-beta
-  region     = var.region
+  project      = var.project_id
+  provider     = google-beta
+  region       = var.region
   api_config   = google_api_gateway_api_config.api_cfg.id
   gateway_id   = var.gateway_id
   display_name = var.gateway_name
